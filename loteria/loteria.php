ed44ebc12699168d9ac41da7556f01be12961825
Goldy005 <bhupinder7727@gmail.com>
2019-10-20 11:10:09 +0200

    loteria terminada

--- loteria/loteria.php
+++ loteria/loteria.php
@@ -1,37 +1,67 @@
 <?php 
+/*
+    Nombre:Shajinder Singh
+    Curso: 2DAW DUAL
+    Proyecto:loteria
 
+*/
+
+//clase
     class App{
 
+        
+        //constructor
         public function __construct(){
             session_start();
+            $_SESSION['apuestaAlmacen'] = array();
         }
 
+        //página princiapl
         public function index(){
             require "loteria.html";
         }
 
+        //métodp para almacenar y eliminar los números selecionados.
         public function toggle(){
             
             $numero = $_GET['number'];
 
+            
+            //si están seleccionados los borra.
             if(isset($_SESSION['apuesta'][$numero])){
                 unset($_SESSION['apuesta'][$numero]);
+                //si están seleccionados los almacena 
             }else{
                 $_SESSION['apuesta'][$numero]=$numero;
             }
 
             $_SESSION['numerosSelecionados']=sizeof($_SESSION['apuesta']);
+            
             header('Location: /loteria/loteria.php?method=index');
         }
 
+        //metódo para reiniciar la apuesta, almacena la apuesta
         public function flush(){
 
-            array_push($_SESSION['apuestaAlmacen[]'],$_SESSION['apuesta']);
+            //solo alamacena si están seleccionados 6 números
+            if(sizeof($_SESSION['apuesta']) == 6 ){
+
+                array_push($_SESSION['apuestaAlmacen'],$_SESSION['apuesta']);
+                unset($_SESSION['apuesta']);
+                $_SESSION['message'] = "Apuesta almacenada correctamente";
+
+            }else{
+                $_SESSION['message'] = "Error la apuesta debe tener selecionados por lo mínimo 6 números.";
+            }
+
             
             header('Location: /loteria/loteria.php?method=index');
 
         }
         
+    }
+
+    //main
 
         $app = new App();
 
